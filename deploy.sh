1f83d0fa69aee1eabcb0f81afea4d799cad61b1b
Kavindulakmal <avakavindu@gmail.com>
2025-02-01 13:12:12 +0530

    complete project

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+
+
+pwd
+
+# run maven
+./mvnw clean install -DskipTests
+
+#mv  target/tmachine-lambda-proxy-final.jar deploy/tmachine-lambda-proxy-final.jar
+
+#cd deploy
+
+#sls deploy --aws-profile synpulse
